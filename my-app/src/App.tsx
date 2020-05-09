2d4e2cd193d54d8aeb212748b41d49e5f601606e
Bibiana Souza <souzabibiana@hotmail.com>
2020-05-09 13:56:10 -0700

    added CSS and the delete function to each to do list item

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,13 +7,24 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+    document.querySelector("form")?.reset();
+  };
+
+  const deleteTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId); //filter method is built in JS and filters through an array.
+      //It drops the item which returns false for the check and returns all others
+    });
   };
 
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={deleteTodoHandler} />
     </div>
   );
 };
--- my-app/src/components/NewTodo.tsx
+++ my-app/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
 interface NewTodoProps {
   onAddTodo: (todoText: string) => void;
@@ -8,7 +9,7 @@ const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null); //when the component gets rendered for the first time, the form input hasnt rendered yet so you're passing a dummy value
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    if (textInputRef.current != null && textInputRef.current.value != "") {
+    if (textInputRef.current !== null && textInputRef.current.value !== "") {
       const enteredText = textInputRef.current.value;
       props.onAddTodo(enteredText);
     } else alert("You must provide a new To Do!");
@@ -16,7 +17,7 @@ const NewTodo: React.FC<NewTodoProps> = (props) => {
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text"> New Todo </label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
